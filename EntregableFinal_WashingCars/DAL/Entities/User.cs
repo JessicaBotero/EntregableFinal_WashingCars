4610e4e9e6e6eb2f002fbfb3383a9e78bbc7f1e4
JESSICA MARIA BOTERO AGUDELO <jessicabotero130348@correo.itm.edu.co>
2023-11-19 16:18:41 -0500

    creating a list of user types through the Enum type class

---
+++ EntregableFinal_WashingCars/DAL/Entities/User.cs
@@ -0,0 +1,59 @@
+﻿using EntregableFinal_WashingCars.Enum;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EntregableFinal_WashingCars.DAL.Entities
+{
+    public class User : IdentityUser
+    {
+
+        [Display(Name = "Documento")]
+        [MaxLength(10, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        
+        public string Document { get; set; }
+        
+
+        [Display(Name = "Nombres")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string FirstName { get; set; }
+
+
+        [Display(Name = "Apellidos")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string LastName { get; set; }
+
+
+        [Display(Name = "Número de Teléfono")]
+        [MaxLength(10, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int PhoneNumber { get; set; }
+
+
+        [Display(Name = "Foto")]
+        public Guid ImageId { get; set; }
+
+        
+        [Display(Name = "Foto")]
+        public string ImageFullPath => ImageId == Guid.Empty
+            ? $"https://localhost:7158/images/NoImage.png"
+            : $"https://sales2023.blob.core.windows.net/users/{ImageId}";
+
+
+        [Display(Name = "Tipo de usuario")]
+        public UserType UserType { get; set; }
+
+
+        //Propiedades de Lectura --- No se muestran en base de datos por que son solo lectura
+        [Display(Name = "Usuario")]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Display(Name = "Usuario")]
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+
+
+    }
+}
