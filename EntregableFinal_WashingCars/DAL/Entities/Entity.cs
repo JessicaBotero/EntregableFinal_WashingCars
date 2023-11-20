b9c4fe5201576fa940fa36e4477ff02da12838e3
Daniel Ciro <danielciro312@gmail.com>
2023-11-20 18:37:09 -0500

    Created Class Entity

---
+++ EntregableFinal_WashingCars/DAL/Entities/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntregableFinal_WashingCars.DAL.Entities
+{
+    public class Entity
+    {
+
+        [Required]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Fecha de Creación")]
+        public DateTime? CreatedDate { get; set; }
+
+        [Display(Name = "Fecha de Modificación")]
+        public DateTime? ModifiedDate { get; set; }
+
+    }
+}
