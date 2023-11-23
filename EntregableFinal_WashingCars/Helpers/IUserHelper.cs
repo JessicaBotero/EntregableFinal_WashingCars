1f4229ba3dfbba1cbe9a72ffc8d6ced4faee9ef4
JESSICA MARIA BOTERO AGUDELO <jessicabotero130348@correo.itm.edu.co>
2023-11-23 11:18:28 -0500

    Interfaz IUserHelper Created

---
+++ EntregableFinal_WashingCars/Helpers/IUserHelper.cs
@@ -0,0 +1,18 @@
+﻿using EntregableFinal_WashingCars.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace EntregableFinal_WashingCars.Helpers
+{
+    public interface IUserHelper
+    {
+        Task<User> GetUserAsync(string email); //Captura los usuarios desde la BD
+
+        Task<IdentityResult> AddUserAsync(User user, string password);// Adiciona los usuarios a la BD
+
+        Task AddRoleAsync(string roleName); // Agrega los roles a las tablas de BD (Admin, Client) 
+
+        Task AddUserToRoleAsync(User user, string roleName); // Relaciona la tabla User con Roles
+
+        Task<bool> IsUserInRoleAsync(User user, string rolName); // Valida si el usuario ingresado esta relacionado con un rol
+    }
+}
